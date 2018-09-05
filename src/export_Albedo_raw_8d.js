fb2ff6011e859c3e42357f51f6a16e601ea13e60
kongdd <kongdd@live.cn>
2018-09-05 02:18:18 +0000

    Updated src/export_Albedo_raw_8d.js (added 44 lines, deleted 5 lines).

--- src/export_Albedo_raw_8d.js
+++ src/export_Albedo_raw_8d.js
@@ -1,6 +1,7 @@
 /**** Start of imports. If edited, may not auto-convert in the playground. ****/
 var imgcol_gpp = ee.ImageCollection("MODIS/006/MOD17A2H"),
-    imgcol_v2 = ee.ImageCollection("projects/pml_evapotranspiration/PML/OUTPUT/PML_V2_8day");
+    imgcol_v2 = ee.ImageCollection("projects/pml_evapotranspiration/PML/OUTPUT/PML_V2_8day"),
+    imageCollection = ee.ImageCollection("MODIS/006/MCD43A3");
 /***** End of imports. If edited, may not auto-convert in the playground. *****/
 var pkg_trend  = require('users/kongdd/public:Math/pkg_trend.js');
 var pkg_smooth = require('users/kongdd/public:Math/pkg_smooth.js');
@@ -11,8 +12,8 @@ var range = [-180, -60, 180, 90];
 var bound = ee.Geometry.Rectangle(range, 'EPSG:4326', false);
 
 var nday       = 32; // interpolation searching 32d in the left and right
-var year_begin = 2013,
-    year_end   = year_begin + 4;
+var year_begin = 2012,
+    year_end   = year_begin + 5;
 
 var md_begin = (year_begin === 2002) ? '-07-04' : '-01-01';
 var date_begin = ee.Date(year_begin.toString().concat(md_begin)).advance(-nday, 'day'),
@@ -34,7 +35,7 @@ var Albedo_d8 = pkg_trend.aggregate_prop(Albedo_raw, 'dn', 'median')
     .map(function(img){ return img.multiply(0.001).copyProperties(img, img.propertyNames()); })
     .map(pkg_trend.add_dn(false, 8))
     .select([0], ['Albedo']);
-// print(Albedo_d8.limit(3))
+print(Albedo_d8.limit(3))
 // Map.addLayer(Albedo_d8.limit(92), {}, 'albedo raw');
 
 var Emiss_d8 = ee.ImageCollection('MODIS/006/MOD11A2')
@@ -125,7 +126,45 @@ var range      = [-180, -60, 180, 90], // keep consistent with modis data range
 print('prjs', prj_albedo, prj_emiss)
 // pkg_export.ExportImgCol(imgcol_out.limit(10), dateList, range, cellsize, type, folder, 
 //     crs, prj.crsTransform);
+
+var imgcol = Albedo_d8.map(function(img){
+    return img.multiply(1000).toInt16().copyProperties(img, img.propertyNames());
+});
+// Map.addLayer(imgcol.limit(50), {}, 'Albedo_d8')
+
 folder = 'projects/pml_evapotranspiration/PML_INPUTS/MODIS/v012/Albedo_raw_8d';
-pkg_export.ExportImgCol(Albedo_d8, dateList, range, cellsize, type, folder, 
+pkg_export.ExportImgCol(imgcol, dateList, range, cellsize, type, folder, 
     crs, prj_albedo.crsTransform);
+
+//////////////////////////// MAIN FUNCTIONS ////////////////////////////////////
+function get_chart(imgcol, name){
+  var point = /* color: #d63000 */ee.Geometry.Point([-104.48822021484375, 65.42901140039487]);
+
+  var chart = ui.Chart.image.series({
+      imageCollection: imgcol, //['ETsim', 'Es', 'Eca', 'Ecr', 'Es_eq']
+      region         : point,
+      reducer        : ee.Reducer.mean(),
+      scale          : 500
+  });
+  print(chart, name);
+}
+
+/** count images number */
+function count_imgcol(imgcol){
+    var img_count = imgcol.count();
+    // img_count = img_count.mask(img_count.lt(46));
+    return img_count;
+}
+
+// scale:0.002  offset:0.49
+function zip_emiss(img){
+    var x = img.expression('(b(0) - 0.49)*500');
+    return img.select('qc').addBands(x).toUint8();
+}
+
+// scale:0.001
+function zip_albedo(img){
+    var x = img.expression('b(0) * 1000').toUint16();
+    return img.select('qc').toUint8().addBands(x);
+}
 
