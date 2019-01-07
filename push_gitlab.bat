5beeed438ce44456d034503bf6db64fe5388020b
Dongdong Kong <kongdd@users.noreply.github.com>
2019-01-07 23:29:34 +0800

    Update README.md

--- push_gitlab.bat
+++ push_gitlab.bat
@@ -1,4 +1,5 @@
-git remote set-url origin https://gitlab.com/hydro2/gee_PML
+rem git remote set-url origin https://gitlab.com/hydro2/gee_PML
+git remote set-url origin https://github.com/kongdd/PML
 git push -f
 
 git remote set-url origin https://earthengine.googlesource.com/users/kongdd/gee_PML
