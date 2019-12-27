d10683448ce9a703e0787c8d70944576dc195a2c
kongdd.sysu <kongdd.sysu@gmail.com>
2019-12-27 10:02:36 +0000

    Updated src/Aggregate_8day_To_Yearly.js (added 25 lines, deleted 14 lines).

--- src/Aggregate_8day_To_Yearly.js
+++ src/Aggregate_8day_To_Yearly.js
@@ -1,14 +1,20 @@
+/**** Start of imports. If edited, may not auto-convert in the playground. ****/
+var imgcol_land = ee.ImageCollection("MODIS/006/MCD12Q1");
+/***** End of imports. If edited, may not auto-convert in the playground. *****/
 /**
  * Dongdong Kong, 20191227
  * 
  * Aggregate into yearly
  */
-var imgcol_PMLV2_v015_8d = ee.ImageCollection('projects/pml_evapotranspiration/PML/V2/8day')
+var pkg_export = require('users/kongdd/public:pkg_export.js');
+var imgcol_PMLV2_v015_8d = ee.ImageCollection('projects/pml_evapotranspiration/PML/V2/8day');
 
 
-var imgcol_years = aggregateToYearly(imgcol_PMLV2_v015_8d, 2003, 2018, true);
+var imgcol_years = aggregateToYearly(imgcol_PMLV2_v015_8d, 2003, 2018, 0.01, true);
 
 var exec = true;
+
+var prj = pkg_export.getProj(imgcol_land);
 var range     = [-180, -60, 180, 90],
     bounds    = ee.Geometry.Rectangle(range, 'EPSG:4326', false), //[xmin, ymin, xmax, ymax]
     cellsize  = 1 / 240, //1/240,
@@ -17,6 +23,7 @@ var range     = [-180, -60, 180, 90],
     folder    = 'projects/pml_evapotranspiration/PML/V2/yearly',
     crsTransform = prj.crsTransform;
 
+// Map.addLayer(imgcol_years, {}, 'yearly');
 pkg_export.ExportImgCol(imgcol_years, null, range, cellsize, type, folder, crs, crsTransform);
 
 
@@ -24,7 +31,8 @@ pkg_export.ExportImgCol(imgcol_years, null, range, cellsize, type, folder, crs, 
 function aggregateToYearly(imgcol, year_begin, year_end, scale_factor, verbose) {
     scale_factor = scale_factor || 0.01;
     if (verbose === undefined) verbose = false;
-
+    
+    var bands = ['GPP', 'Ec', 'Es', 'Ei', 'ET_water', 'qc']; //,'qc'
     var years = ee.List.sequence(year_begin, year_end);
 
     var imgcol_years = years.map(function(year) {
@@ -33,16 +41,19 @@ function aggregateToYearly(imgcol, year_begin, year_end, scale_factor, verbose) 
 
         var ydays = date_begin.advance(1, 'year').difference(date_begin, 'day');
         var imgcol_year = imgcol.filterDate(date_begin, date_end);
-
-        imgcol_PML.select(bands.slice(0, -1))
-            .multiply(scale_factor).mean()
-            .multiply(ydays)
+        var scale = ydays.multiply(scale_factor);
+        
+        return imgcol_year.select(bands.slice(0, -1))
+            // .multiply(scale_factor).
+            .mean()
+            .multiply(scale)
             .toFloat()
-            .set('system:time_start', begin_date.millis())
-            .set('system:id', begin_date.format());
-    })
-
-    if (verbose) print("imgcol_years", imgcol_years)
+            .set('system:time_start', date_begin.millis())
+            .set('system:id', date_begin.format());
+    });
+    
+    imgcol_years = ee.ImageCollection(imgcol_years);
+    if (verbose) print("imgcol_years", imgcol_years);
 
     // pkg_export.ExportImg(img_year, task, range, cellsize, type, folder_yearly, crs, crsTransform);
     return imgcol_years;
